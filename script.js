3b0031f827ec6262e9303cf4a1c292efbde0793f
Narender Singh bisht <89627914+narendercoder@users.noreply.github.com>
2022-03-11 20:32:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 // Game Constants & Variables
 let inputDir = {x: 0, y: 0}; 
-const foodSound = new Audio('food.mp3');
-const gameOverSound = new Audio('gameover.mp3');
-const moveSound = new Audio('move.mp3');
-const musicSound = new Audio('music.mp3');
+let foodSound = new Audio('food.mp3');
+let gameOverSound = new Audio('gameover.mp3');
+let moveSound = new Audio('move.mp3');
+let musicSound = new Audio('music.mp3');
 let board = document.querySelector('.board');
 let music = document.getElementById('music');
 let speed = 8;
